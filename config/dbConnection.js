62501f1597e49dda2f7e7532f2dc2bad00725295
Ze Maria <zemaria@MacBook-Pro-de-Ze.local>
2025-03-24 18:33:39 -0300

    atualizaindo conexcao

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,35 +1,22 @@
 import mysql from 'mysql2' ;
 
-// const dbConnect = function() {
-//     return mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: '53364430187',
-//         database: 'cstbalancas',
-//         port: 3306
-//     });
-// } 
-// export default function() {
-//     return dbConnect()
-// }
-
 //CRIAR CONECCAO COM O DB  
- const db = mysql.createConnection({
+  const dbConnect = mysql.createConnection({
             host: 'mysql.cstbalancas.com.br',
             user: 'cstbalancas',
             password: 'Cstwild533644',
             database: 'cstbalancas',
             port: 3306,
             multipleStatements: true
-});
+    });
 
 // const dbConnect = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '53364430187',
-//     database: 'cstbalancas',
-//     port: 3306
-// })
+//      host: 'localhost',
+//      user: 'root',
+//      password: '53364430187',
+//      database: 'cstbalancas',
+//      port: 3306
+//  })
 
 export default dbConnect;
 
